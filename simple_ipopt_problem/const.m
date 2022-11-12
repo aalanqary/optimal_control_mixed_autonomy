242e43844722a8a677476c3d9b7f52a93fc36750
Ashish Pandian <ashishpandian@berkeley.edu>
2022-11-11 23:46:17 -0800

    ipopt

---
+++ simple_ipopt_problem/const.m
@@ -0,0 +1,53 @@
+function [c, ceq] = const(U, auxdata)  
+    % c(x, u) <= 0 
+    % ceq(x, u) = 0
+    c = [];
+    [time_v, v] = system_solve(U, auxdata); 
+    v = griddedInterpolant(time_v, v, "previous");
+    v = v(auxdata.tau);
+    
+    if isrow(v)
+        v = v';
+    end 
+    if isrow(U)
+        U = U'; 
+    end 
+    
+    if false
+        % First constraint (eq): v(T) = 0  
+        ceq(1) = v(end); 
+        
+        c = [U - auxdata.g - auxdata.k3 * v.^2; % Second constraint (ineq): u(t) <= g + k3 * v(t)
+            - U - auxdata.g - auxdata.k3 * v.^2]; % Third constraint (ineq): u(t) >= -g - k3 * v(t)
+    end 
+
+    if false
+        % First constraint (eq): v(T) = 0  
+        ceq(1) = v(end); 
+        % Second constraint (ineq): u(t) <= g + k3 * v(t)
+        h1 = auxdata.g + auxdata.k3 * v - U;
+        ceq(2) = sum(min(h1, 0).^2);
+        % Third constraint (ineq): u(t) >= -g - k3 * v(t)
+        h2 =  auxdata.g + auxdata.k3*v.^2 + U; 
+        ceq(3) = sum(min(h2, 0).^2);
+
+        c = [];
+    end 
+
+    % Using Traditional Approach
+    if true
+        % First constraint (eq): v(T) = 0  
+        ceq(1) = v(end); 
+        % Second constraint (ineq): u(t) <= g + k3 * v(t)
+        h1 = auxdata.g + auxdata.k3 * v.^2 - U;
+        bound_h1 = (-auxdata.eps <= h1) + (auxdata.eps >= h1);
+        pi_1 = h1 .* (h1 < -auxdata.eps) - (((h1 - auxdata.eps).^2)/4*auxdata.eps) .* (bound_h1 >= 2);
+        c(1) = - auxdata.gamma - trapz(auxdata.tau, pi_1);
+        % Third constraint (ineq): u(t) >= -g - k3 * v(t)
+        h2 =  auxdata.g + auxdata.k3*v.^2 + U; 
+        bound_h2 = (-auxdata.eps <= h2) + (auxdata.eps >= h2);
+        pi_2 = h2 .* (h2 < -auxdata.eps) - ((h2 - auxdata.eps).^2/4*auxdata.eps) .* (bound_h2 >= 2);
+        c(2) = - auxdata.gamma - trapz(auxdata.tau, pi_2);
+    end 
+
+end
---
+++ simple_ipopt_problem/objective.m
@@ -0,0 +1,5 @@
+function f = objective(U, auxdata)  
+    [time_v, v] = system_solve(U, auxdata);
+    f = -trapz(time_v, v);
+end
+
