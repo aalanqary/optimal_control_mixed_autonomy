4f1d1976a93726392faad4ca5fa6b292ae4284ab
Anish Gollakota <anishgollakota@Anishs-MacBook-Pro.local>
2023-01-30 19:44:24 -0800

    Debugged errors in objective gradient. Issue with scalar in objective

--- simple_problem/obj_grad_penalty.m
+++ simple_problem/obj_grad_penalty.m
@@ -3,17 +3,15 @@ function df = obj_grad_penalty(U, auxdata)
     %[dp1_dv, dp2_dv] = dp_dv(U, v, auxdata);
     %[dp1_du, dp2_du] = dp_du(U, v, auxdata);
     v = griddedInterpolant(time_v, v, "linear");
+    U = griddedInterpolant(auxdata.tau(1:end-1), U(1:end-1), "linear");
     % Interpolating dp_du
     %dp1_du_interp = griddedInterpolant(time_v, dp1_du, "linear")
     %dp2_du_interp = griddedInterpolant(time_v, dp2_du, "linear")
     lambdaT = 0;
-    %length(1 + lambdaT *(auxdata.k1 + 2*auxdata.k2*v(0)) + auxdata.gamma*(dp1_du + dp2_du))
-    %length(lambdaT)
     Lambda = ode5(@(t,lambda) helper(U(t), v(t), lambda, auxdata), flip(time_v), lambdaT);
-    size(Lambda)
     Lambda = flip(Lambda);
     Lambda = griddedInterpolant(time_v, Lambda, "linear");
-    df = arrayfun(@(a,b) -1*integral(@(t) -1 * auxdata.gamma*(dp1_du + dp2_du) + Lambda, a, b), auxdata.tau(1:end-1), auxdata.tau(2:end));  
+    df = arrayfun(@(a,b) integral(@(t) helper_2(U(t), v(t), Lambda(t), auxdata), a, b), auxdata.tau(1:end-1), auxdata.tau(2:end));  
     if isrow(df)
         df = df';
     end 
@@ -45,3 +43,24 @@ end
 function h = helper(u, v, lambda, auxdata)
     h = 1 + lambda*(auxdata.k1 + 2*auxdata.k2*v) + auxdata.gamma*(dp1_dv(u, v, auxdata) + dp2_dv(u, v, auxdata));
 end
+
+function f = dp1_du(u, v, auxdata)
+    h = auxdata.g + auxdata.k3*v.^2 - u; 
+    bound_h = (-auxdata.eps <= h) + (auxdata.eps >= h);
+    f = -1 * (h < -auxdata.eps) + ...
+        (-(1/4*auxdata.eps) * (2 * u - 2*(auxdata.g + auxdata.k3*v.^2 - auxdata.eps))) .* (bound_h >= 2); 
+end
+
+function f = dp2_du(u, v, auxdata)
+    h = auxdata.g + auxdata.k3*v.^2 + u; 
+    bound_h = (-auxdata.eps <= h) + (auxdata.eps >= h);
+    f = 1 * (h < -auxdata.eps) + ...
+        (-(1/4*auxdata.eps) * (2 * u + 2*(auxdata.g + auxdata.k3*v.^2 - auxdata.eps))) .* (bound_h >= 2); 
+end
+
+function h = helper_2(u, v, lambda, auxdata)
+    h = -1 * auxdata.gamma*(dp1_du(u, v, auxdata) + dp2_du(u, v, auxdata)) + lambda;
+end
+
+
+
--- simple_problem/objective_penalty.m
+++ simple_problem/objective_penalty.m
@@ -1,10 +1,11 @@
 function f = objective_penalty(U, auxdata)  
     [time_v, v] = system_solve(U, auxdata);
+    %v = griddedInterpolant(time_v, v, "previous");
 
     % Extracting rho from helper functions
     pi_1 = p_1(U, v, auxdata);
     pi_2 = p_2(U, v, auxdata);
 
-    %f = -trapz(time_v, v) - auxdata.gamma * (trapz(auxdata.tau, pi_1) + trapz(auxdata.tau, pi_2));
     f = -trapz(time_v, v) - auxdata.gamma * (trapz(time_v, pi_1) + trapz(time_v, pi_2));
+    class(f)
 end
